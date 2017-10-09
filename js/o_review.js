3c0c3f7256fe742eda3ec957ed0e9dd70ae6c394
zhjm <943257156@qq.com>
2017-10-09 09:08:31 +0800

    订单审核修改

--- js/o_review.js
+++ js/o_review.js
@@ -10,69 +10,68 @@ $('.table-sort').dataTable({
 		//{"bVisible": false, "aTargets": [ 3 ]} //控制列的隐藏显示
 		{
 			"orderable": false,
-			"aTargets": [0,10]
+			"aTargets": [0]
 		} // 不参与排序的列
 	]
 });
-/*添加*/
-function article_add() {
-}
 
-/*编辑*/
-function article_edit(obj) {
-	var window_title='编辑';
-	var title=['订单信息','客户信息','产品信息'];
-	var content_title=[
-		['客户名称','订单时间','开单人员','产品','数量','价格','快递方式','备注'],
-		['单位名称','地址','联系人','电话','客户类型','客户价格','管辖业务员','当前欠款'],
-		['品牌','型号','片型','折算率','销售价格']
-	];
-	var content_type=[
-		['text','time','text','text','number','text','text','text'],
-		['text','text','text','text','text','text','text','text'],
-		['select','text','text','text','text']
-	];
-	var content_id=[
-		['customer_name','order_time','billing_staff','product','price','express','remarks'],
-		['company_name','add','name','tel','customer_type','customer_price','clerk','arrears'],
-		['brand','model','chip','conversion','price']
-	];
-	var select_content=[['品牌1','品牌2','品牌3']];
-	edit_large_show(window_title,title,content_title,content_type,content_id,'1200px','auto',select_content);
-	verification_val(content_id[0][0],'isBlank');
-}
-/*删除*/
-function article_del(obj, id) {
-	layer.confirm('确认要删除吗？', function(index) {
-		/*$.ajax({
-			type: 'POST',
-			url: '',
-			dataType: 'json',
-			success: function(data) {
-				$(obj).parents("tr").remove();
-				layer.msg('已删除!', {
-					icon: 1,
-					time: 1000
-				});
-			},
-			error: function(data) {
-				console.log(data.msg);
-			},
-		});*/
-		$(obj).parents("tr").remove();
-		layer.msg('已删除!', {
-			icon: 1,
-			time: 1000
-		});
-	});
-}
 
-$('.edit_btn').on('click', function() {
-	article_edit($(this));
-});
-$('.add_btn').on('click', function() {
-	article_add();
-});
-$('.del_btn').on('click', function() {
-	article_del($(this))
+var setting = {
+	view: {
+		dblClickExpand: false,
+		showLine: false,
+		selectedMulti: false
+	},
+	data: {
+		simpleData: {
+			enable:true,
+			idKey: "id",
+			pIdKey: "pId",
+			rootPId: ""
+		}
+	},
+	callback: {
+		beforeClick: function(treeId, treeNode) {
+			var zTree = $.fn.zTree.getZTreeObj("tree");
+			if (treeNode.isParent) {
+				zTree.expandNode(treeNode);
+				return false;
+			} else {
+				demoIframe.attr("src",treeNode.file + ".html");
+				return true;
+			}
+		}
+	}
+};
+
+var zNodes =[
+	{ id:1, pId:1, name:"2017-10-09", open:true},
+		{ id:111, pId:1, name:"KZ-0001-130410-006"},
+		{ id:112, pId:1, name:"KZ-0001-130410-007"},
+		{ id:113, pId:1, name:"KZ-0001-130410-008"},
+		{ id:114, pId:1, name:"KZ-0001-130410-009"},
+		{ id:115, pId:1, name:"KZ-0001-130410-010"},
+	{ id:2, pId:2, name:"2017-10-10", open:true},
+		{ id:211, pId:2, name:"KZ-0001-130410-006"},
+		{ id:212, pId:2, name:"KZ-0001-130410-007"},
+		{ id:213, pId:2, name:"KZ-0001-130410-008"},
+		{ id:214, pId:2, name:"KZ-0001-130410-009"},
+		{ id:215, pId:2, name:"KZ-0001-130410-010"},
+];
+		
+var code;
+		
+function showCode(str) {
+	if (!code) code = $("#code");
+	code.empty();
+	code.append("<li>"+str+"</li>");
+}
+		
+$(document).ready(function(){
+	var t = $("#treeDemo");
+	t = $.fn.zTree.init(t, setting, zNodes);
+	demoIframe = $("#testIframe");
+	//demoIframe.on("load", loadReady);
+	var zTree = $.fn.zTree.getZTreeObj("tree");
+	//zTree.selectNode(zTree.getNodeByParam("id",'11'));
 });
